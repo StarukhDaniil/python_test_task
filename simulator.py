9716b4deedac324eec7226b2a311d76cda01834f
StarukhDaniil <starukh.danil@gmail.com>
2025-11-13 16:02:22 +0200

    Added draft version of simulator.py

--- simulator.py
+++ simulator.py
@@ -1,15 +1,14 @@
 from pymavlink import mavutil
-import time
 
-def f():
-    print("simulator started")
-    time.sleep(1)
-    print("simulator ended")
-
-# def simulate(tlog_path):
-#     log = mavutil.mavlink_connection(tlog_path)
-
-#     while True:
-#         msg = log.recv_match()
-#         if msg is None:
-#             break
+class Simulator:
+    def __init__(self, tlog_path, port):
+        self.__log_connection = mavutil.mavlink_connection(tlog_path)
+        self.__handler_connection = mavutil.mavlink_connection(f"tcpout:127.0.0.1:{port}")
+    
+    def simulate(self):
+        while True:
+            msg = self.__log_connection.recv_match()
+            if msg is None:
+                break;
+            raw_bytes = msg.get_msgbuf()
+            self.__handler_connection.write(raw_bytes)
